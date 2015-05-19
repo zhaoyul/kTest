2f230ef764cdb116b166064a7aa54c86e7dbf59c
Kevin.Li <li_zhaoyu@msn.com>
2015-05-19 08:37:24 +0800

    Initial Commit

---
+++ Motion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Motion
+//
+//  Created by Zhaoyu Li on 5/19/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Motion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Motion
+//
+//  Created by Zhaoyu Li on 5/19/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
